adb9d82f28fb2c052050e3d1b99927b9fe5d1d7e
unknown <aperan09@gmail.com>
2024-10-18 16:02:38 +0200

    First commit

---
+++ main.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+int number(int a){
+  return a;
+}
+
+int main (int argc, char** argv){
+
+  printf("Hello\n");
+  printf("Hello it is me\n");
+  printf("%d\n", number(5));
+
+  if(2!=2){
+    return 1;
+  }
+
+
+  return 0;
+
+}
